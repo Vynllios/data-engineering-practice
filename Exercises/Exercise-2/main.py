3427471612f697437eeb266065f9a6c44a74627a
Erick Stubblefield <erick.stubb@gmail.com>
2022-06-05 20:36:21 -0400

    Exercise 2 complete

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,12 +1,28 @@
 import requests
-import pandas
-
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 def main():
-    # your code here
+    site_url='https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    r=requests.get(site_url)
+    soup=BeautifulSoup(r.text, 'html.parser')
+    tags=soup.find_all('tr')
+    for tag in tags:
+        if '2022-02-07 14:03 ' in tag.get_text():
+            filename=tag.a['href']
+            break
+    csv_url=site_url+filename
+
+    df=pd.read_csv(csv_url,header=0)
+    value=df['HourlyDryBulbTemperature'].max()
+    result_df=df.loc[df['HourlyDryBulbTemperature']==value]
+    print('\nResult dataframe :\n',
+          result_df)
+
     pass
 
 
 if __name__ == '__main__':
     main()
+
